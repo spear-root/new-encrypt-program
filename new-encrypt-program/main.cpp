7b38c26d51340aad678df08ab3b23b90339fbd73
spear-root <spear.root@gmail.com>
2017-02-01 00:16:42 +0900

    Original source code of min050820's Pull Request (#1,  09/10/2016)

--- new-encrypt-program/main.cpp
+++ new-encrypt-program/main.cpp
@@ -1,58 +1,77 @@
-#include "D:\B.C.K\Dropbox\H\ustd\ustd v4.5.10.h"
+//#include "D:\B.C.K\Dropbox\H\ustd\ustd v4.5.10.h" //You should open this header file is you're willing to ipen this code.
+#include <cstdio>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
-char buf[4096 + 5];
+string inputfile = "input.txt", outputfile = "";
+char buf[4096];
 vector<char> original;
+bool mode_interactive;
 
-void encrypt()
-{
-	// Write the code
+const char str_hello[] = "Hello! Welcome to <put this program name here>!";
+const char str_request_inputfile[] = "Input file name (including path) : ";
+const char str_request_outputfile[] = "Output file name (Empty to assume filename.enc)";
+const char str_processing[] = "Encrypting the file... Might take some time.";
+const char str_finished[] = "Encryption is finished now!";
+
+void parse_parm(int argc, char* argv[]) {
+	for (int i = 1; i<argc; i++) {
+		if (strcmp(argv[i], "--interactive") == 1)
+			mode_interactive = true;
+		else if (strcmp(argv[i], "--non-interactive") == 1)
+			mode_interactive = false;
+	}
 }
 
-void decrypt()
-{
-	// Write the code
+//Encrypt one byte of data.
+inline char encrypt(const char data) {
+	//TODO: Implement encryption
 }
 
-int main()
+//Process one block(4096 bytes) of file.
+void process(FILE* r, FILE* w) {
+	const int size = fread((void*)buf, sizeof(char), 4096, r);
+	for (register int i = 0; i<size; i++)
+		buf[i] = encrypt(i);
+	fwrite(buf, sizeof(char), size, w);
+}
+
+int main(int argc, char* argv[])
 {
-	// Input
-	FILE *r, *w;
-	r = fopen("input.txt", "rb");
-	if (r == NULL)
-	{
-		_D_RED;
-		perror("input.txt");
-		_RESET;
-		return EXIT_FAILURE;
+	parse_parm(argc, argv);
+	if (mode_interactive) {
+		cout << str_hello << endl;
+		cout << str_request_inputfile;
+		cin.getline(buf);
+		inputfile = buf;
+		cout << str_request_outputfile;
+		cin.getline(buf);
+		outputfile = buf;
 	}
+	if (outputfile == "")
+		outputfile = inputfile + ".enc";
 
-	//for (asdf)  // 파일 크기가 4kB 넘을 경우 for문으로 4kB씩 잘라 받는 코드 추가 예정.
-	{
-		int len = fread(buf, sizeof(char), 4096, r);
-
-		original.resize(len);
-		// char[]에 들어있는 바이너리 데이터를 vector<char>으로 옮긴다
-		for (int i = 0; i < len; i++)
-		{
-			original[i] = buf[i];
-		}
+	//Input
+	FILE * r, w;
+	r = fopen(inputfile.c_str(), "rb");
+	if (r == 0) {
+		cerr << "Unable to open input file!" << endl;
+		return 1;
 	}
+	w = fopen(outputfile.c_str(), "wb");
 
+	//Encrypt
+	cout << str_processing;
+	while (!feof(r))
+		process(r, w);
+	cout << endl;
 
-	// Encrypt
-	//encrypt();
-
-
-	// Decrypt (for Debug)
-	//decrypt();
-
-
-	// Output
-	w = fopen("output.txt", "wb");
-	fwrite(&original[0], sizeof(char), original.size(), stdout);// w);
-
+	//Close
 	fclose(r);
 	fclose(w);
+	if (mode_interactive)
+		cout << str_finished << endl;
 	return 0;
 }
